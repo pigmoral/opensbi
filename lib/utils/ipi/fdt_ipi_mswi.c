f3ed7a595f300f52bf58865928d037ddf168a91b
Junhui Liu <junhui.liu@pigmoral.tech>
2025-10-16 14:21:27 +0800

    lib: utils/ipi: mswi: add Nuclei UX900 compatible
    The timer unit in the Nuclei UX900 is compliant with the ACLINT
    specification. Add the compatible string for its MSWI part.
    
    Signed-off-by: Junhui Liu <junhui.liu@pigmoral.tech>

--- lib/utils/ipi/fdt_ipi_mswi.c
+++ lib/utils/ipi/fdt_ipi_mswi.c
@@ -57,6 +57,7 @@ static const struct fdt_match ipi_mswi_match[] = {
 	{ .compatible = "sifive,clint0", .data = &clint_offset },
 	{ .compatible = "thead,c900-clint", .data = &clint_offset },
 	{ .compatible = "thead,c900-aclint-mswi" },
+	{ .compatible = "nuclei,ux900-aclint-mswi" },
 	{ .compatible = "riscv,aclint-mswi" },
 	{ },
 };
